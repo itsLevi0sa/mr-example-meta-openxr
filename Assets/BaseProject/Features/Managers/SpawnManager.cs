2d8ec0808f7b9ce14bc8baf3ea41e6bcf56e48c7
elli <furioza1234@gmail.com>
2024-03-31 06:17:07 +0300

    numeric keypad done

--- Assets/BaseProject/Features/Managers/SpawnManager.cs
+++ Assets/BaseProject/Features/Managers/SpawnManager.cs
@@ -6,14 +6,24 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject xrKeyboard;
     [SerializeField] Transform target;
-    [SerializeField] Vector3 offsetFromTarget;
+    //[SerializeField] Vector3 offsetFromTarget;
 
     [ContextMenu("Spawn XR Keyboard")]
     public void XRKeyboardSpawner()
     {
-        xrKeyboard.transform.position = target.transform.position+offsetFromTarget;
+        //xrKeyboard.transform.position = target.transform.position+offsetFromTarget;
         xrKeyboard.SetActive(true);
         xrKeyboard.GetComponent<Animator>().SetTrigger("SpawnObject");
         xrKeyboard.GetComponent<AudioSource>().Play();
+    }
+
+    [ContextMenu("Despawn XR Keyboard")]
+    public void XRKeyboardDespawner()
+    {
+        //xrKeyboard.transform.position = target.transform.position+offsetFromTarget;
+        
+        xrKeyboard.GetComponent<Animator>().SetTrigger("DespawnObject");
+        xrKeyboard.GetComponent<AudioSource>().Play();
+        xrKeyboard.SetActive(false);
     }
 }
--- Assets/MRExampleAssets/Scripts/PositionAtStart.cs
+++ Assets/MRExampleAssets/Scripts/PositionAtStart.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.Content.UI.Layout;
 
 public class PositionAtStart : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("The starting target.")]
     Transform m_Target;
+    Vector3 initialPos;
+    public float lerpDuration = 1f; // Duration of interpolation
+    public LazyFollower lazyFollower;
 
     [SerializeField]
     [Tooltip("Adjusts the follow point from the target by this amount.")]
     Vector3 m_TargetOffset = Vector3.forward;
 
     bool m_IgnoreX = true;
+    bool startFollow = false;
     Vector3 m_TargetPosition;
 
     private const float k_StartDelay = 2.0f;
 
-    // Start is called before the first frame update
-    IEnumerator Start()
+    private void Start()
     {
-        yield return new WaitForSeconds(k_StartDelay);
+        initialPos = this.gameObject.transform.position;
+    }
+
+    [ContextMenu("StartFollow")]
+    public void StartFollow()
+    {
+        StartCoroutine(DelayFollow());
+    }
+
+    IEnumerator DelayFollow()
+    {
+        float elapsedTime = 0f;
+
         var targetRotation = m_Target.rotation;
         var newTransform = m_Target;
         var targetEuler = targetRotation.eulerAngles;
@@ -32,6 +48,49 @@ public class PositionAtStart : MonoBehaviour
 
         newTransform.rotation = targetRotation;
         m_TargetPosition = m_Target.position + newTransform.TransformVector(m_TargetOffset);
+
+        this.GetComponent<Animator>().SetTrigger("SpawnObject");
+        this.GetComponent<AudioSource>().Play();
+
+        while (elapsedTime < lerpDuration)
+        {
+            // Increment elapsedTime over time
+            elapsedTime += Time.deltaTime;
+
+            // Calculate interpolation factor
+            float t = Mathf.Clamp01(elapsedTime / lerpDuration);
+
+            // Perform linear interpolation between start and end positions
+            transform.position = Vector3.Lerp(initialPos, m_TargetPosition, t);
+
+            yield return null; // Wait for the next frame
+        }
+
+        // Ensure the final position is exactly at the end position
         transform.position = m_TargetPosition;
+        //yield return new WaitForSeconds(lerpDuration);
+        //startFollow = true;
+        lazyFollower.enabled = true;
+    }
+
+    public void Update()
+    {
+        if (startFollow)
+        {
+            var targetRotation = m_Target.rotation;
+            var newTransform = m_Target;
+            var targetEuler = targetRotation.eulerAngles;
+            targetRotation = Quaternion.Euler
+            (
+                m_IgnoreX ? 0f : targetEuler.x,
+                targetEuler.y,
+                targetEuler.z
+            );
+
+            newTransform.rotation = targetRotation;
+            m_TargetPosition = m_Target.position + newTransform.TransformVector(m_TargetOffset);
+            transform.position = m_TargetPosition;
+        }
+        
     }
 }
